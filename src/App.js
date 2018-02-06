d308d036d42bcd996d9d2a18b20cd8ac72fd9078
shachi <bhavsarshachi@gmail.com>
2018-02-06 12:58:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import data from './data.js';
 import Drag from './Drag';
-import jsondata from './data.json'
 import {JsonTree} from 'react-editable-json-tree'
+import jsonData from './data.json'
 
 class App extends Component {
   constructor(props){
@@ -26,7 +26,7 @@ class App extends Component {
     return (
       <div style={{backgroundColor : 'black'}}>
         <div style={{float:'left',width:"50%",backgroundColor:"#85DAB9"}}>
-            <Drag data ={data}/>
+            <Drag data={jsonData}/>
         </div>
         <div style={{float:'right',width:"45%",backgroundColor:'#CEF7E4'}}>
             <JsonTree data={data}  
