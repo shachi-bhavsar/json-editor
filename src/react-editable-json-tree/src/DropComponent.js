cddc5d49d8118cac538fc1f54ac5e5de43fdaa00
shachi <bhavsarshachi@gmail.com>
2018-02-07 12:27:13 +0530

    Update DropComponent.js

--- src/react-editable-json-tree/src/DropComponent.js
+++ src/react-editable-json-tree/src/DropComponent.js
@@ -1,18 +1,43 @@
 
 import React from 'react';
-
 export default class DropComponent extends React.Component {
     
-    onKeyPress(e){
-        e.preventDefault();
+    constructor(props){
+        super(props);
+        this.state = {
+            container : []
+        }
+        this.onKeyPress = this.onKeyPress.bind(this);
     }
 
+    onDrop = e => {
+        //const data = e.dataTransfer.getData("text/json")
+        var data = e.dataTransfer.getData("text/plain");
+        let {container} = this.state;
+        container.push(data);
+        this.setState({container});
+        console.log("state: "+this.state.container);
+    }  
+
+    allowDrop = ev => {
+        ev.preventDefault();
+    }
+
+    onKeyPress(e){
+        if(e.type === 'mousedown')
+        {
+            console.log("mouse used");
+            return;
+        }
+        if(e.type === 'keypress')
+        {
+            e.preventDefault();
+            return;
+        }
+    }
     render(){
-        
-        return(
-            <div>                
-                <input style={{backgroundColor:"yellow"}} onKeyPress={this.onKeyPress}/>
-            </div>
+        return(               
+                <input onChange={this.onKeyPress} value={this.state.container} ref={this.state.container} onDragOver={this.allowDrop} onDrop={this.onDrop}/>
         )
     }
 }
