26f41951a26af7e9b6f824ba80f353279be56b25
shachi <bhavsarshachi@gmail.com>
2018-02-08 16:38:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,27 +4,30 @@ import {JsonTree} from './react-editable-json-tree/src/JsonTree'
 import jsonData from './masterConfig.json'
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      data : {data}
-    }
+        data: {data},
+    };
     this.onFullyUpdate = this.onFullyUpdate.bind(this);
   }
 
   onFullyUpdate(data) {
+
+    let updatedData = JSON.stringify(data);
       this.setState({
           data : {data},
       });
+      console.log(updatedData);
   }
-
+ 
   render() {
     return (
       <div style={{backgroundColor : 'black'}}>
         <div style={{float:'left',width:"50%",backgroundColor:"#85DAB9"}}>            
             <JsonTree data={jsonData}
-            readOnly={true}
-            isCollapsed = {(keyPath, deep) => (deep !== 0)}
+              readOnly={true}
+              isCollapsed = {(keyPath, deep) => (deep !== 0)}
             />
         </div>
         <div style={{float:'right',width:"45%",backgroundColor:'#CEF7E4'}}>
